4c5466e8b916d915554935a0bf2a340c2e608f3a
Spencer Longhurst <spencerlonghurst@Spencers-MacBook-Pro.local>
2022-03-08 21:06:40 -0700

    Made the axios calls cleaner

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -14,11 +14,12 @@ export default class App extends React.Component {
     const { value } = evt.target;
     this.setState({ ...this.state, todoNameInput: value })
   }
+  resetForm = () => this.setState({ ...this.state, todoNameInput: ''})
   postNewTodo = () => {
     axios.post(URL, { name: this.state.todoNameInput })
     .then(res => {
-      this.fetchAllTodos()
-      this.setState({ ...this.state, todoNameInput: ''})
+      this.setState({ ...this.state, todos: this.state.todos.concat(res.data.data) })
+      this.resetForm()
     })
     .catch(err => {
       this.setState({ ...this.state, error: err.response.data.message })
