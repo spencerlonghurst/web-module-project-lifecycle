7105c759b6edc9e8b7506b1c8135069dc0ef0f2c
Spencer Longhurst <spencerlonghurst@Spencers-MacBook-Pro.local>
2022-03-08 22:07:49 -0700

    Added the TodoList component into seperate file

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 import Form from './Form';
+import TodoList from './TodoList';
 
 const URL = 'http://localhost:9000/api/todos'
 
@@ -73,17 +74,11 @@ export default class App extends React.Component {
     return (
       <div>
         <div id="error">Error: {this.state.error}</div>
-        <div id="todos">
-          <h2>Todos:</h2>
-          {
-            this.state.todos.reduce((acc, td) => {
-              if (this.state.displayCompleteds || !td.completed) return acc.concat(
-                <div onClick={this.toggleCompleted(td.id)} key={td.id}>{td.name}{td.completed ? ' ✔️' : ''}</div>
-              )
-              return (acc)
-            }, [])
-          }
-        </div>
+        <TodoList
+          toggleCompleted={this.toggleCompleted}
+          todos={this.state.todos}
+          displayCompleteds={this.state.displayCompleteds}
+        />
         <Form
           onTodoFormSubmit={this.onTodoFormSubmit}
           onTodoNameInputChange={this.onTodoNameInputChange}
